456d57fc5e24ab306a0ef3f66c53ee99ef52863c
ChandupaSH <chandupash107@gmail.com>
2023-12-09 16:24:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import jsQR from 'jsqr';
 
-function App() {
+const QrScanner = () => {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    const initCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+        videoRef.current.srcObject = stream;
+      } catch (error) {
+        console.error('Error accessing camera:', error);
+      }
+    };
+
+    initCamera();
+  }, []);
+
+  const handleScan = () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+    
+    if (!video.videoWidth || !video.videoHeight) {
+      // Wait for video dimensions to be available
+      requestAnimationFrame(handleScan);
+      return;
+    }
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const code = jsQR(imageData.data, imageData.width, imageData.height, {
+      inversionAttempts: 'dontInvert',
+    });
+
+    if (code) {
+      setResult(code.data);
+    }
+
+    requestAnimationFrame(handleScan);
+  };
+
+  useEffect(() => {
+    handleScan();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video ref={videoRef} autoPlay playsInline muted />
+      <canvas ref={canvasRef} style={{ display: 'none' }} />
+      {result && <p>QR code scanned: {result}</p>}
     </div>
   );
-}
+};
 
-export default App;
+export default QrScanner;
